eca5b0d09153abd123e6fc327e57fbb691f0f071
Tom Jenkinson <tom@tjenkinson.me>
2021-02-24 22:09:12 +0000

    add prettier config in index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./.eslintrc.js');
+const baseConfig = require('./.eslintrc.json');
+
+module.exports = {
+  ...baseConfig,
+  rules: {
+    ...baseConfig.rules,
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: "always",
+        printWidth: 100,
+        singleQuote: true,
+        trailingComma: "none"
+      }
+    ],
+  }
+};
